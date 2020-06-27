4c2284c41e4c1c694bbfe5579ffc7226f10b8251
Nikoloz Pirtskhalava <n.phirtskhalava@bog.ge>
2020-06-27 16:38:03 +0400

    Initial Commit

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Nikoloz Phirtskhalava on 6/27/20.
+//  Copyright © 2020 Nikoloz Phirtskhalava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
